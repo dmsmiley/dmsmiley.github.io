19b1606e6eee61ac95cf7758d7a9af04422b3b0e
David M. Smiley <davidmsmiley@gmail.com>
2021-03-03 20:57:17 -0500

    Update data_science_chart.js

--- assets/js/data_science_chart.js
+++ assets/js/data_science_chart.js
@@ -23,6 +23,6 @@ anychart.onDocumentReady(function() {
   chart.title("The deadliest earthquakes in the XXth century");
 
   // draw
-  chart.container("container");
+  chart.container("chartContainer");
   chart.draw();
 });
